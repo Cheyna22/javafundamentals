65ea6c335c0498c1771c1852fecc4f2ccc0ce342
Cheyna22 <humphries.c22@gmail.com>
2018-11-27 13:54:50 -0800

    had to take out concatenation for the period in order to work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static String pluralize(String petName, int num) {
         if(num == 0 ||  num > 1){
-            petName = petName + 's';
+            petName = petName + "s";
             return petName;
         } else {
             return petName;
@@ -11,12 +11,12 @@ public class Main {
 
     public static void main(String[] args) {
         int dogCount = 1;
-        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount + "."));
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount));
 
         int catCount = 2;
-        System.out.println("I own " + catCount + " " + pluralize("cat", catCount + "."));
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount));
 
         int turtleCount = 0;
-        System.out.println("I own " + turtleCount + " " + pluralize("dog", turtleCount + "."));
+        System.out.println("I own " + turtleCount + " " + pluralize("dog", turtleCount));
     }
 }
