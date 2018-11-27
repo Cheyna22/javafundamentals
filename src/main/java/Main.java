32c6eb72a3a4884975f8841262098d155a37adaf
Cheyna22 <humphries.c22@gmail.com>
2018-11-27 13:48:23 -0800

    moved code to ij

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static String pluralize(String petName, int num) {
+        if(num == 0 ||  num > 1){
+            petName = petName + 's';
+            return petName;
+        } else {
+            return petName;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount + "."));
+
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount + "."));
+
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("dog", turtleCount + "."));
+    }
+}
