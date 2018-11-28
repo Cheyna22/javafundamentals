7b70120210c2e0756d7cc68901c527a54561bd2c
Cheyna22 <humphries.c22@gmail.com>
2018-11-27 16:27:01 -0800

    pushing through any minor changes as we went through code review in class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
 public class Main {
+//    Checks if num is equal to 0 or greater than 1
     public static String pluralize(String petName, int num) {
         if(num == 0 ||  num > 1){
-            petName = petName + "s";
+            petName = petName + 's';
             return petName;
         } else {
             return petName;
         }
-
     }
 
+    
+
+
+//    Test cases to run method and see if it behaves correctly, put all test cases in this method
     public static void main(String[] args) {
         int dogCount = 1;
         System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount));
@@ -18,5 +22,6 @@ public class Main {
 
         int turtleCount = 0;
         System.out.println("I own " + turtleCount + " " + pluralize("dog", turtleCount));
+
     }
 }
