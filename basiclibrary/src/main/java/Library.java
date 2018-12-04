19b0756c80db4e248f6760636eab95d27c5a6e9e
Cheyna22 <humphries.c22@gmail.com>
2018-12-04 15:08:51 -0800

    worked on analyze weather data, not complete

--- basiclibrary/src/main/java/Library.java
+++ basiclibrary/src/main/java/Library.java
@@ -59,4 +59,36 @@ public class Library {
         }
         return lowestAverageSoFar;
     }
+
+//    Lab3 Maps =========================================================================================================
+
+    public static int analyzeWeatherData(int[][] monthTempsArray){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        // variables to store the min/max temps for each array
+        int minTempSoFar = Integer.MIN_VALUE;
+        int maxTempSoFar = Integer.MAX_VALUE;
+
+        // iterate through the arrays to find the min temp
+        for(int i = 0; i < weeklyMonthTemperatures.length; i++) {
+            for(int j = i +1; j < monthTempsArray.length; j++) {
+                if(minTempSoFar  weeklyMonthTemperatures[i])
+            }
+        }
+    }
+    // iterate through arrays
+    // find the min value - store it
+    // find the max value - store it
+    // Use a HashSet of type Integer HashSet<Integer>
+        // to keep track of all unique temps seen
+    // iterate from min temp to max temp
+        // print out any temps not seen during month
+
+    public static List<String votes> tally(){
+       // accepts a list of Strings
+    }
 }
